148a74a6138ff834f4f3c7b51bc9bfbde8d1f7ca
CocoDev <c.biti12@gmail.com>
2019-05-13 15:01:42 +0200

    Scripts: Implment Flying Wave Spheres

---
+++ Assets/_Own/Scripts/FlyingSphere.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class FlyingSphere : MonoBehaviour
+{
+    [Header("Movement Settings")] 
+    [SerializeField] float flyingSpeed = 1.5f;
+
+    [Header("Scaling Settings")]
+    [SerializeField] float scaleTarget = 0.8f;
+    [SerializeField] float scaleDuration = 0.7f;
+    [SerializeField] float scaleRandomMin = 0.4f;
+
+    [Header("Other Settings")]
+    [SerializeField] float delayToDespawn = 5.0f;
+    [SerializeField] float delayToRevealDots = 2.0f;
+    [SerializeField] float targetSphereRadius = 0.25f;
+
+    private SphereCollider sphereCollider;
+    public RadarHighlightLocation highlightLocation { get; set; }
+
+    private void Start()
+    {
+        sphereCollider = GetComponent<SphereCollider>();
+        Assert.IsNotNull(sphereCollider);
+
+        // Make sure the collider matches the scale as this script will be attached to a sphere.
+        sphereCollider.radius = transform.localScale.x;
+        
+        // Set the rotation to face the camera position + some kind of sphere offset.
+        var targetPosition = Camera.main.transform.position;
+        targetPosition += Random.insideUnitSphere * targetSphereRadius;
+        
+        transform.rotation = Quaternion.LookRotation(targetPosition - transform.position);
+        
+        // Randomize scale over time, also adjust the collider radius.
+        float randomScale = scaleTarget * Mathf.Max(Random.value, scaleRandomMin);
+        transform.DOScale(new Vector3(randomScale, randomScale, randomScale), scaleDuration);
+
+        DOTween.To( 
+            () => sphereCollider.radius, 
+            x => sphereCollider.radius = x, 
+            randomScale, 
+            scaleDuration);
+        
+        // Despawn it after some time and reveal the position spawned from. (TODO: this will be moved to when it hits the actual hands)
+        this.Delay(delayToDespawn, () => { Destroy(gameObject); });
+        this.Delay(delayToRevealDots, () => { DotsManager.instance.Highlight(highlightLocation); });
+    }
+
+    private void Update() => transform.position += flyingSpeed * Time.deltaTime * transform.forward;
+}
--- Assets/_Own/Scripts/TestDots.cs
+++ Assets/_Own/Scripts/TestDots.cs
@@ -10,7 +10,8 @@ public class TestDots : MonoBehaviour
 
     [SerializeField] float dotEmissionConeAngle = 40.0f;
     [SerializeField] float maxDotDistanceFromSurfacePointAlongOriginalRayDirection = 1.0f;
-
+    [SerializeField] GameObject flyingSpherePrefab;
+    
     IEnumerator Start()
     {
         DisableAllRenderers();
@@ -30,9 +31,10 @@ public class TestDots : MonoBehaviour
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
-        
-        if (Input.GetKeyDown(KeyCode.Space))
-            Probe(new Ray(transform.position, transform.forward));
+
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            Probe(new Ray(transform.position, transform.forward), true);
+        }
     }
 
     private void ProbeFrom(Camera cam)
@@ -60,19 +62,37 @@ public class TestDots : MonoBehaviour
                 foundRenderer.enabled = false;
     }
 
-    private void Probe(Ray ray)
+    private void Probe(Ray ray, bool spawnFlyingSphere = false)
     {
         RaycastHit hit;
         bool didHit = Physics.SphereCast(ray, sphereCastRadius, out hit);
         if (!didHit)
             return;
 
-        DotsManager.instance.Highlight(new RadarHighlightLocation
+        RadarHighlightLocation highlightLocation = new RadarHighlightLocation
         {
             originalRay = ray,
             pointOnSurface = hit.point,
             dotEmissionConeAngle = dotEmissionConeAngle,
-            maxDotDistanceFromSurfacePointAlongOriginalRayDirection = maxDotDistanceFromSurfacePointAlongOriginalRayDirection
-        });
+            maxDotDistanceFromSurfacePointAlongOriginalRayDirection =
+                maxDotDistanceFromSurfacePointAlongOriginalRayDirection
+        };
+
+        if (spawnFlyingSphere)
+        {
+            Assert.IsNotNull(flyingSpherePrefab);
+
+            GameObject flyingSphere = Instantiate(
+                flyingSpherePrefab,
+                hit.point,
+                Quaternion.identity);
+
+            FlyingSphere flyingSphereComp = flyingSphere.GetComponent<FlyingSphere>();
+            flyingSphereComp.highlightLocation = highlightLocation;
+        }
+        else {
+            // TODO: For testing purposes move this out of this block to paint the dots when space is hit.
+            DotsManager.instance.Highlight(highlightLocation);
+        }
     }
 }
