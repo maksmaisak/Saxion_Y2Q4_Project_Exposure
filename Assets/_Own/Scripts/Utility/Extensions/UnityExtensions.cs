77f1d61eecc4c72e57d947fd7bcb9f7af8926f4a
CocoDev <c.biti12@gmail.com>
2019-05-14 11:38:21 +0200

    Scripts/Extensions: Create a LayerMask extensions to check if it contains a specific layer

---
+++ Assets/_Own/Scripts/Utility/Extensions/UnityExtensions.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public static class UnityExtensions
+{
+    /// <summary>
+    /// Extension method to check if a layer is in a layermask
+    /// </summary>
+    /// <param name="mask"></param>
+    /// <param name="layer"></param>
+    /// <returns></returns>
+    public static bool ContainsLayer(this LayerMask mask, int layer) => mask == (mask | (1 << layer));
+
+}
