cb7f2e34c7d95e868649dbefee8cf6713cf468cc
RTX_ON <max.maisak@gmail.com>
2019-05-14 15:23:27 +0200

    Scripts: Implement radar gun with pressable trigger

--- Assets/VRTK/LegacyExampleFiles/ExampleResources/Scripts/Gun.cs
+++ Assets/VRTK/LegacyExampleFiles/ExampleResources/Scripts/Gun.cs
@@ -11,22 +11,10 @@
         public override void StartUsing(VRTK_InteractUse usingObject)
         {
             base.StartUsing(usingObject);
-            FireBullet();
         }
 
         protected void Start()
         {
-            bullet = transform.Find("Bullet").gameObject;
-            bullet.SetActive(false);
-        }
-
-        private void FireBullet()
-        {
-            GameObject bulletClone = Instantiate(bullet, bullet.transform.position, bullet.transform.rotation) as GameObject;
-            bulletClone.SetActive(true);
-            Rigidbody rb = bulletClone.GetComponent<Rigidbody>();
-            rb.AddForce(-bullet.transform.forward * bulletSpeed);
-            Destroy(bulletClone, bulletLife);
         }
     }
 }
---
+++ Assets/_Own/Scripts/Radar/RadarController.cs
@@ -0,0 +1,33 @@
+﻿using DG.Tweening;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class RadarController : VRTK.VRTK_InteractableObject
+{
+    [Header("Radar Settings")]
+    [SerializeField] GameObject wavePulsePrefab;
+
+    public override void StartUsing(VRTK.VRTK_InteractUse usingObject)
+    {
+        base.StartUsing(usingObject);
+        Debug.Log("Radar fired!");
+
+        CreateWavePulse();
+    }
+
+    private void CreateWavePulse()
+    {
+        Assert.IsNotNull(wavePulsePrefab);
+
+        const float duration = 2.0f;
+
+        GameObject pulse = Instantiate(wavePulsePrefab, transform.position, Quaternion.identity);
+        Transform tf = pulse.transform;
+
+        tf.localScale = Vector3.zero;
+        tf.DOScale(20, duration).SetEase(Ease.Linear);
+
+        Destroy(pulse, duration);
+    }
+}
+
