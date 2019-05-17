d7a1bacff72daf7968ad3ce246c7120c0e568be4
Maks <max.maisak@gmail.com>
2019-05-17 19:52:04 +0200

    Testing: added the large cavernous cave prefab into TestDots_FlyingWaveSphere scene

--- Assets/_Own/Scripts/Radar/DotsRegistry.cs
+++ Assets/_Own/Scripts/Radar/DotsRegistry.cs
@@ -17,7 +17,7 @@ public class DotsRegistry
     
     public int totalNumDots { get; private set; }
 
-    public DotsRegistry(float cellSize = 2.0f)
+    public DotsRegistry(float cellSize = 1.0f)
     {
         this.cellSize = cellSize;
         sceneBounds = GetSceneBounds();
