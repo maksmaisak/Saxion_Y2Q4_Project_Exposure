55c98ba0e2f261573bcc9bad154955ea9d078f49
Maks <max.maisak@gmail.com>
2019-05-28 22:10:53 +0200

    Pointer: right-hand pointer only.
    Also removed extraneous code from HandsController

--- Assets/_Own/Scripts/HandsController.cs
+++ Assets/_Own/Scripts/HandsController.cs
@@ -4,18 +4,15 @@ using VRTK.Examples;
 
 public class HandsController : ToggleCustomHands, IEventReceiver<OnRevealEvent>, IEventReceiver<OnTeleportEvent>
 {
-    private VRTK_Pointer leftPointer;
-    private VRTK_Pointer rightPointer;
-    
-    private VRTK_StraightPointerRenderer leftPointerRenderer;
-    private VRTK_StraightPointerRenderer rightPointerRenderer;
+    private VRTK_Pointer pointer;
     
     protected void Start() => EventsManager.instance.Add(this);
 
     protected void OnDestroy()
     {
         var manager = EventsManager.instance;
-        if (manager) manager.Remove(this);
+        if (manager) 
+            manager.Remove(this);
     }
 
     protected override void OnEnable()
@@ -34,39 +31,16 @@ public class HandsController : ToggleCustomHands, IEventReceiver<OnRevealEvent>,
 
     private void AssignControllerPointers()
     {
-        leftPointer = leftController.GetComponent<VRTK_Pointer>();
-        rightPointer = rightController.GetComponent<VRTK_Pointer>();
-        leftPointerRenderer = leftController.GetComponent<VRTK_StraightPointerRenderer>();
-        rightPointerRenderer = rightController.GetComponent<VRTK_StraightPointerRenderer>();
-
-        ToggleRenderer(false);
+        pointer = rightController.GetComponent<VRTK_Pointer>();
+        pointer.Toggle(false);
     }
-
-    private void ToggleRenderer(bool newState, bool bothHands = false)
-    {
-        rightPointer.enableTeleport = newState;
-
-        VRTK_BasePointerRenderer.VisibilityStates visibilityState = newState
-            ? VRTK_BasePointerRenderer.VisibilityStates.AlwaysOn
-            : VRTK_BasePointerRenderer.VisibilityStates.AlwaysOff;
-        
-        rightPointerRenderer.tracerVisibility = visibilityState;
-        rightPointerRenderer.cursorVisibility = visibilityState;
-
-        if (!bothHands && newState) return;
-        
-        leftPointer.enableTeleport = newState;
-        
-        leftPointerRenderer.tracerVisibility = visibilityState;
-        leftPointerRenderer.cursorVisibility = visibilityState;
-    }
-
-    public void On(OnRevealEvent reveal) => ToggleRenderer(true);
+    
+    // TODO only toggle if it's the current section getting revealed
+    public void On(OnRevealEvent reveal) => pointer.Toggle(true);
 
     public void On(OnTeleportEvent teleport)
     {
         LightSection lightSection = teleport.navpoint.GetComponentInParent<LightSection>();
-        
-        ToggleRenderer(lightSection && lightSection.isRevealed);
+        pointer.Toggle(lightSection && lightSection.isRevealed);
     }
 }
