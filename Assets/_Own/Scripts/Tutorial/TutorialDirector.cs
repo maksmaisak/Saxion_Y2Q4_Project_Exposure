7f8ecb939c8161a550f9e765c3eef603a01055a9
Maks <max.maisak@gmail.com>
2019-06-01 19:25:35 +0200

    Tutorial: automatic rotation, pulsing and grabability of the radar

--- Assets/_Own/Scripts/Tutorial/TutorialDirector.cs
+++ Assets/_Own/Scripts/Tutorial/TutorialDirector.cs
@@ -1,24 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.Assertions;
 
 public class TutorialDirector : MonoBehaviour
 {
     [SerializeField] RadarController radarController;
-    
+
+    private RadarTool radarTool;
+
     IEnumerator Start()
     {
+        EnsureIsInitializedCorrectly();
+
+        radarController.isGrabbable = false;
+
+        yield return new WaitForSeconds(2.0f);
+
+        yield return radarController.transform
+            .DORotate(Vector3.up * 90.0f, 5.0f)
+            .WaitForCompletion();
+
+        radarTool.Probe();
+
+        yield return radarController.transform
+            .DORotate(Vector3.up * -90.0f, 5.0f)
+            .WaitForCompletion();
+
+        radarTool.Probe();
+
+        yield return radarController.transform
+            .DORotate(Vector3.zero, 5.0f)
+            .WaitForCompletion();
+
+        radarController.isGrabbable = true;
+    }
+
+    private void EnsureIsInitializedCorrectly()
+    {
         Assert.IsNotNull(radarController);
-        
-        radarController.enabled = false;
-
-        yield return new WaitForSeconds(5.0f);
-        
-        radarController.GetComponent<RadarTool>().Probe();
-            
-        yield return new WaitForSeconds(5.0f);
-
-        radarController.enabled = true;
+        radarTool = radarController.GetComponent<RadarTool>();
+        Assert.IsNotNull(radarTool);
     }
 }
