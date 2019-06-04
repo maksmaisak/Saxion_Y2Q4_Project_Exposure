33f2fbc88195971248bd69cb4651d8c364452596
CocoDev <c.biti12@gmail.com>
2019-06-04 15:12:58 +0200

    Scripts/Radar: Make the pointer snap to the navpoint when close

--- Assets/_Own/Scripts/Radar/RadarPointerController.cs
+++ Assets/_Own/Scripts/Radar/RadarPointerController.cs
@@ -5,10 +5,32 @@ using VRTK;
 
 public class RadarPointerController : VRTK_StraightPointerRenderer
 {
+    [Header("Laser Settings")]
+    [SerializeField] LayerMask laserPointerCollisionLayer;
+    
     protected override void CreatePointerOriginTransformFollow()
     {
         base.CreatePointerOriginTransformFollow();
 
         pointerOriginTransformFollow.moment = VRTK_TransformFollow.FollowMoment.OnLateUpdate;
+    }
+
+    protected override void SetPointerAppearance(float tracerLength)
+    {
+        base.SetPointerAppearance(tracerLength);
+
+        if (actualContainer != null)
+        {
+            if (destinationHit.collider != null)
+            {
+                if (!laserPointerCollisionLayer.ContainsLayer(destinationHit.collider.gameObject.layer))
+                    return;
+                
+                actualContainer.transform.rotation = Quaternion.LookRotation(
+                    destinationHit.collider.gameObject.transform.position - actualContainer.transform.position);
+            }
+            else
+                actualContainer.transform.rotation = Quaternion.identity;
+        }
     }
 }
