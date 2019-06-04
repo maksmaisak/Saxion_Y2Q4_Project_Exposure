329567ed076c7b2f71f2143aa5d1c80bc0f81c30
CocoDev <c.biti12@gmail.com>
2019-06-04 14:54:20 +0200

    Scripts/Radar: Fix radar pointer lagging follow movement

---
+++ Assets/_Own/Scripts/Radar/RadarPointerController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class RadarPointerController : VRTK_StraightPointerRenderer
+{
+    protected override void CreatePointerOriginTransformFollow()
+    {
+        base.CreatePointerOriginTransformFollow();
+
+        pointerOriginTransformFollow.moment = VRTK_TransformFollow.FollowMoment.OnLateUpdate;
+    }
+}
