7813724b74dde06e94d4fef0324c77015640cd9b
Popa <georgeantonio1998@yahoo.com>
2019-06-05 22:37:17 +0200

    Wave-Sphere: Made the wave sphere pulse and play a sound like the nav points do

---
+++ Assets/_Own/Scripts/FlyingSphereTutorial.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+using DG.Tweening;
+public class FlyingSphereTutorial : MonoBehaviour
+{
+    [SerializeField]AudioSource audioSourceSecoundary;
+    [SerializeField] AudioClip pulseSound;
+    
+    [SerializeField] float pulsePunchScale = 0.1f;
+    [SerializeField] float pulseDuration   = 1.0f;
+    [SerializeField] float pulseInterval   = 0.3f;
+    [SerializeField] int   pulseElasticity = 1;
+    
+    private void Start()
+    {
+        audioSourceSecoundary = GetComponent<AudioSource>();
+        PlaySequence();
+    }
+
+    private void PlaySequence()
+    {
+        transform.DOKill();
+        DOTween.Sequence()
+            .AppendCallback(() => {
+                audioSourceSecoundary.clip = pulseSound;
+                audioSourceSecoundary.loop = false;
+                audioSourceSecoundary.Play();
+            })
+            .Append(transform.DOPunchScale(Vector3.one * pulsePunchScale, pulseDuration, pulseElasticity))
+            .AppendInterval(pulseInterval)
+            .SetLoops(-1, LoopType.Restart);
+    }
+}
