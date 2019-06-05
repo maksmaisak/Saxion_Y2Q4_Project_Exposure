37e980986e03716a06f58e3cc8afcd4e878e50bb
CocoDev <c.biti12@gmail.com>
2019-06-05 21:46:18 +0200

    Scripts/Radar: Remove pointer snapping mechanic

--- Assets/_Own/Scripts/Radar/RadarPointerController.cs
+++ Assets/_Own/Scripts/Radar/RadarPointerController.cs
@@ -14,23 +14,4 @@ public class RadarPointerController : VRTK_StraightPointerRenderer
 
         pointerOriginTransformFollow.moment = VRTK_TransformFollow.FollowMoment.OnLateUpdate;
     }
-
-    protected override void SetPointerAppearance(float tracerLength)
-    {
-        base.SetPointerAppearance(tracerLength);
-
-        if (actualContainer != null)
-        {
-            if (destinationHit.collider != null)
-            {
-                if (!laserPointerCollisionLayer.ContainsLayer(destinationHit.collider.gameObject.layer))
-                    return;
-                
-                actualContainer.transform.rotation = Quaternion.LookRotation(
-                    destinationHit.collider.gameObject.transform.position - actualContainer.transform.position);
-            }
-            else
-                actualContainer.transform.localRotation = Quaternion.identity;
-        }
-    }
 }
