0559cc3cbc13bda0f3dc0a9bbb01066d46275345
CocoDev <c.biti12@gmail.com>
2019-06-07 14:43:05 +0200

    Scripts: Make the animation where the DishHolder opens and the gun slides out

---
+++ Assets/_Own/Scripts/OnTutorialEnd.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.Events;
+
+public class OnTutorialEnd : MyBehaviour, IEventReceiver<OnTutorialEndEvent>
+{
+    [SerializeField] GameObject leftCabinet;
+    [SerializeField] GameObject rightCabinet;
+    [SerializeField] GameObject radarGun;
+    [SerializeField] float openCabinetDuration = 1.5f;
+    [SerializeField] float slideOutDuration = 1.5f;
+    [SerializeField] float slideOutZOffset = 0.3f;
+
+    private void Start()
+    {
+        Assert.IsNotNull(radarGun);
+        Assert.IsNotNull(leftCabinet);
+        Assert.IsNotNull(rightCabinet);
+    }
+    
+    public void On(OnTutorialEndEvent tutorialEndEvent)
+    {
+        DOTween.Sequence()
+            .Join(leftCabinet.transform.DORotate(new Vector3(0, -90.0f, 0), openCabinetDuration).SetEase(Ease.InQuart))
+            .Join(rightCabinet.transform.DORotate(new Vector3(0, 90.0f, 0), openCabinetDuration).SetEase(Ease.InQuart))
+            .OnComplete(() =>
+            {
+                float posZ = radarGun.transform.position.z;
+                radarGun.transform.DOMoveZ(posZ - slideOutZOffset, slideOutDuration);
+            });
+    }
+}
