3777f758206e54ec1565a6323c8e160be8607e64
Maks <max.maisak@gmail.com>
2019-06-10 21:18:50 +0200

    Level: various minor fixes. Increased max num spheres per second from 2 to 3

--- Assets/_Own/Scripts/OnReveal.cs
+++ Assets/_Own/Scripts/OnReveal.cs
@@ -7,14 +7,20 @@ public class OnReveal : MyBehaviour, IEventReceiver<OnRevealEvent>
 {
     [Tooltip("This will happen once the section containing this object is revealed.")]
     public UnityEvent onReveal;
+    [SerializeField] float delay = 1.0f;
 
     public void On(OnRevealEvent reveal)
     {
         // If there is nothing hooked up to this event then there is no reason to continue
         if (onReveal == null)
             return;
-        
-        if (reveal.lightSection.GetGameObjects().Contains(gameObject) || transform.IsChildOf(reveal.lightSection.transform))
+
+        if (!transform.IsChildOf(reveal.lightSection.transform) && !reveal.lightSection.GetGameObjects().Contains(gameObject))
+            return;
+
+        if (delay <= 0.0f)
             onReveal.Invoke();
+        else
+            this.Delay(delay, onReveal.Invoke);
     }
 }
