f650b3c8637e32dcac38b4dfed088d66e0ce212b
Maks <max.maisak@gmail.com>
2019-06-24 17:52:53 +0200

    Infographics: support for any number of crossfading images

---
+++ Assets/_Own/Scripts/Tutorial/Infographics.cs
@@ -0,0 +1,36 @@
+﻿using DG.Tweening;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Infographics : MonoBehaviour
+{
+    [SerializeField] Image[] images;
+    [SerializeField] float crossFadeDuration = 1.0f;
+    [SerializeField] float imageOnScreenDuration = 2.0f;
+    
+    void Start()
+    {
+        DoFadeSequence().SetLoops(-1);
+    }
+
+    private Sequence DoFadeSequence()
+    {
+        Sequence sequence = DOTween.Sequence();
+
+        for (int i = 0; i < images.Length; ++i)
+        {
+            sequence
+                .AppendInterval(imageOnScreenDuration)
+                .Append(DoCrossFade(images[i], images[(i + 1) % images.Length]));
+        }
+
+        return sequence;
+    }
+
+    private Sequence DoCrossFade(Image fadeOut, Image fadeIn)
+    {
+        return DOTween.Sequence()
+            .Join(fadeOut.DOFade(0, crossFadeDuration))
+            .Join(fadeIn.DOFade(1, crossFadeDuration));
+    }
+}
