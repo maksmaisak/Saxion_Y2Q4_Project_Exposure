b2f27d1325aafd79f60ad63fdf2f29f8e70d636a
CocoDev <c.biti12@gmail.com>
2019-06-28 13:33:41 +0200

    Scripts/Wavesphere: Restart the sphere catching melody if the player hasn't caught a wavesphere in a while

--- Assets/_Own/Scripts/WavesphereAudio.cs
+++ Assets/_Own/Scripts/WavesphereAudio.cs
@@ -5,16 +5,25 @@ using UnityEngine;
 public class WavesphereAudio : Singleton<WavesphereAudio>
 {
     [SerializeField] AudioClip[] grabAudioClips;
+    [SerializeField] float delayTillReset = 1.0f;
 
     private int currentClipIndex = 0;
+    private float lastCatchTime;
 
     public AudioClip GetGrabAudioClip()
     {
         if (grabAudioClips == null || grabAudioClips.Length == 0)
             return null;
-        
+
+        if (Time.time - lastCatchTime < lastCatchTime)
+            StopAllCoroutines();
+
         AudioClip clip = grabAudioClips[currentClipIndex];
         currentClipIndex = (currentClipIndex + 1) % grabAudioClips.Length;
+
+        this.Delay(delayTillReset, () => { currentClipIndex = 0; });
+
+        lastCatchTime = Time.time;
         return clip;
     }
 }
