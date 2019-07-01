86057fb4a2a61624553e0c2b47aed89781099516
George <georgeantonio1998@yahoo.com>
2019-07-01 14:59:32 +0200

    Restart-Button: Added restart button functionality and credits

---
+++ Assets/_Own/Scripts/Questionnaire/VRButton.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.Events;
+using VRTK;
+
+public class VRButton : MonoBehaviour
+{
+    public UnityEvent onActivate;
+
+    private bool canBeTouched = true;
+    private bool startHidden = true;
+
+    private Transform lookAtTransform;
+
+    IEnumerator Start()
+    {
+        if (startHidden)
+        {
+            transform.localScale = Vector3.zero;
+            canBeTouched = false;
+        }
+        
+        yield return new WaitUntil(() =>
+        {
+            var camera = Camera.main;
+            if (camera) 
+                lookAtTransform = camera.transform;
+
+            return lookAtTransform;
+        });
+    }
+
+    void LateUpdate()
+    {
+        if (lookAtTransform)
+            transform.LookAt(lookAtTransform);
+    }
+    
+    void OnTriggerEnter(Collider other)
+    {
+        if (!canBeTouched)
+            return;
+        
+        if (!VRTK_PlayerObject.IsPlayerObject(other.gameObject, VRTK_PlayerObject.ObjectTypes.Controller))
+            return;
+
+        canBeTouched = false;
+        
+        onActivate?.Invoke();
+
+        Hide();
+    }
+
+    public void Show()
+    {
+        gameObject.SetActive(true);
+        
+        transform.DOKill();
+        transform
+            .DOScale(1.0f, 0.5f)
+            .SetEase(Ease.OutBack)
+            .OnComplete(() => canBeTouched = true);
+    }
+
+    public void Hide()
+    {
+        canBeTouched = false;
+        
+        transform.DOKill();
+        transform
+            .DOScale(0.0f, 0.5f)
+            .SetEase(Ease.InBack)
+            .OnComplete(() => gameObject.SetActive(false));
+    }
+}
---
+++ Assets/_Own/Scripts/Restart/RestartGame.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.SceneManagement;
+
+public class RestartGame : MonoBehaviour
+{
+    [SerializeField] GameObject objectsParent;
+    [SerializeField] VRButton restartButton;
+    [SerializeField] float startDelay = 4.0f;
+    [SerializeField] float showButtonsDelay = 1.0f;
+    
+    [Header("Debug")] 
+    [SerializeField] bool showOnStart;
+
+    private MyPanel creditsPanel;
+
+
+    private void Awake()
+    {
+        creditsPanel = GetComponentInChildren<MyPanel>();
+        
+        objectsParent.SetActive(true);
+        restartButton.onActivate.AddListener(() => SceneManager.LoadScene(SceneManager.GetActiveScene().name));
+        
+        Assert.IsNotNull(objectsParent);
+        
+        if(!showOnStart)
+            objectsParent.SetActive(false);
+    }
+    
+    private void Play() => StartCoroutine(PlayCoroutine());
+
+    private IEnumerator PlayCoroutine()
+    {
+        yield return new WaitForSeconds(startDelay);
+        
+        creditsPanel.Show();
+        
+        yield return new WaitForSeconds(showButtonsDelay);
+        ShowButton();
+
+    }
+
+    public void Activate()
+    {
+        objectsParent.SetActive(true);
+        Play();
+    }
+    
+    private void ShowButton() => restartButton.Show();
+}
